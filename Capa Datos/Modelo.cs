6b6e94f80594ba4c55f1a94b62808ab9df687af3
Maximiliano-Cardozo <rodrigoparado@gmail.com>
2021-10-09 09:25:49 -0300

    Modelo Souvenir

---
+++ Capa Datos/Modelo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mysql.data.mysqlclient;
+
+
+namespace Capa_Datos
+{
+    public class Modelo
+    {
+        protected string IpDb;
+        protected string NombreDb;
+        protected string UsuarioDb;
+        protected string PasswordDb;
+        protected Mysqk
+
+
+    }
+}
